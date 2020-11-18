d60bdf5f3e30e70586aa5ce7ced5a9998a5ea90f
aarapidis <aarapidis@deloitte.gr>
2020-11-18 19:58:26 +0200

    Created PersonSchema and Person Model

--- myApp.js
+++ myApp.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  age: {type: Number},
+  favoriteFoods: {type: Array}
+});
+
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
